e0ae35d657f390319db06cd84883faca3bc90250
ss-pq <ss-pq@users.noreply.github.com>
2016-09-01 23:32:27 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,9 @@ import PackageDescription
 let package = Package(
     name: "VaporApp",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 17)
+         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 0, minor: 16),
+        .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/CryptoKitten/BCrypt.git", majorVersion: 0, minor: 10)
     ],
     exclude: [
         "Config",
